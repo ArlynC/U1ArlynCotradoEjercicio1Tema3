27300fd1cada793aa2e883b24b6f4ba2a7f304ad
Arlyn Cotrado <alejandra321.any@gmail.com>
2020-01-22 01:12:06 -0500

    formulario material design

--- app/src/main/java/com/example/bottnav/formulario.java
+++ app/src/main/java/com/example/bottnav/formulario.java
@@ -1,21 +1,80 @@
 package com.example.bottnav;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class formulario extends Fragment {
 
+    Spinner mispinner1;
+    String gi;
+    EditText edtgi;
+    EditText etBirthday;
+    Calendar myCalendar = Calendar.getInstance();
 
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            actualizarInput();
+        }
+
+    };
+    private void actualizarInput() {
+        String formatoDeFecha = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(formatoDeFecha, Locale.US);
+
+        etBirthday.setText(sdf.format(myCalendar.getTime()));
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.activity_formulario,container,false);
+        View view = inflater.inflate(R.layout.activity_formulario, container, false);
+
+
+/*
+        mispinner1=(Spinner) view.findViewById(R.id.spgi);
+        edtgi=(EditText)view.findViewById(R.id.edtg);
+        mispinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> arg0,
+                                       View arg1, int position, long arg3) {
+// TODO Auto-generated method stub
+
+                gi=(String) mispinner1.getSelectedItem();
+                edtgi.setText(gi);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+// TODO Auto-generated method stub
+            }
+        });*/
+
 
         return view;
     }
+
+
 }
